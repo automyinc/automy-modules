2558486aad42c2f6ad4996895a775d001448633d
Max Wittal <automyinc@gmail.com>
2018-10-22 21:56:52 +0200

    update

--- dpkg_install_all.sh
+++ dpkg_install_all.sh
@@ -4,17 +4,22 @@ cd $(dirname "$0")
 
 ARCH=$(uname -m)
 
+sudo apt-get remove automy-math automy-basic vnx-web
+
 cd math
+./clean_all.sh
 ./create_debian.sh
 sudo dpkg -i ${ARCH}/automy-math-$(cat VERSION)-${ARCH}.deb
 cd ..
 
 cd basic
+./clean_all.sh
 ./create_debian.sh
 sudo dpkg -i ${ARCH}/automy-basic-$(cat VERSION)-${ARCH}.deb
 cd ..
 
 cd vnx-web
+./clean_all.sh
 ./create_debian.sh
 sudo dpkg -i ${ARCH}/vnx-web-$(cat VERSION)-${ARCH}.deb
 cd ..
