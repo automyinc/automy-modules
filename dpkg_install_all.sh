31a7dd839ae0629ae8bfaa034c8c91c62cf7ff65
Max Wittal <max.wittal@mwittal.de>
2019-03-29 03:51:48 +0100

    auto install vehicle and vision packages also

--- dpkg_install_all.sh
+++ dpkg_install_all.sh
@@ -4,7 +4,7 @@ cd $(dirname "$0")
 
 ARCH=$(uname -m)
 
-sudo apt-get remove automy-math automy-basic
+sudo apt-get remove automy-math automy-basic automy-vehicle automy-vision
 
 cd math
 ./clean_all.sh
@@ -16,5 +16,17 @@ cd basic
 ./clean_all.sh
 ./create_debian.sh
 sudo dpkg -i ${ARCH}/automy-basic-$(cat VERSION)-${ARCH}.deb
+cd ..
+
+cd vehicle
+./clean_all.sh
+./create_debian.sh
+sudo dpkg -i ${ARCH}/automy-vehicle-$(cat VERSION)-${ARCH}.deb
+cd ..
+
+cd vision
+./clean_all.sh
+./create_debian.sh
+sudo dpkg -i ${ARCH}/automy-vision-$(cat VERSION)-${ARCH}.deb
 cd ..
 
